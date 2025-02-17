58d37c6edf4e95531c936bd6c2aaf727e5ae8a51
Josephine <josephinecarrillo@my.whccd.edu>
2025-02-16 23:33:28 -0800

    Update Class1.cs

--- COMP003A.EmployeeManagementSystem/Class1.cs
+++ COMP003A.EmployeeManagementSystem/Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace COMP003A.EmployeeManagementSystem
 {
-    internal class Class1
+    public class Department
     {
+        public string DepartmentName { get; set; }
+
+        public int EmployeeDepartmentId { get; set; }
+
+        public string DepartmentDetails{ get; set; }
+
+        public void SeeEmployeeDepartmentInfo()
+        {
+            Console.WriteLine($"departmet: {DepartmentName}");
+        }
     }
+
+    public class EmployeeHR 
+    {
+
+        public EmployeeHR ()
+        {
+             Console.WriteLine("Human Resources");  
+             Console.WriteLine("HR department manages employee relationships, payrool and benefits.");
+        }
+    }
+
+    public class ITDepartment
+    {
+        public ITDepartment()
+        {
+            Console.WriteLine("Informantion technology");
+            Console.WriteLine("It departement manages company software, hardware, and cybersecurity.");
+        }
+    }
+
 }
